c79b5745f289d20dabfb18e80ae5e1eda2f73cdc
Damian Lavin <damianlavin75@gmail.com>
2024-03-06 12:00:30 -0800

    P4.S4.s4.v5

--- scripts/index.js
+++ scripts/index.js
@@ -49,9 +49,9 @@ function closeModal() {
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
-  let cardImageEl = cardElement.querySelector(".card__image");
-  let cardTitleEl = cardElement.querySelector(".card__title");
-  let cardAltNameEl = cardElement.querySelector(".card__image");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardAltNameEl = cardElement.querySelector(".card__image");
   cardImageEl.src = cardData.link;
   cardTitleEl.textContent = cardData.name;
   cardAltNameEl.alt = cardData.altName;
